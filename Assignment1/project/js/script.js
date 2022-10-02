7b3ad7190609c36c9689ee999867af00a611ff34
langleynick <w0106967@myscc.ca>
2022-10-02 15:48:07 -0400

    Added button
    Added button with link to my repository, using javascript. I also added the meta viewport tag which I had missed before.

---
+++ Assignment1/project/js/script.js
@@ -0,0 +1,5 @@
+"use strict";
+let githubLink = document.getElementById("githubLink");
+githubLink.addEventListener('click', function(){
+    window.open("https://github.com/langleynick/WEB301_Assignments");
+});
