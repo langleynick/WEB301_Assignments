b21f6b1a557580932284ed7be6a3387d49276c17
langleynick <w0106967@myscc.ca>
2022-10-30 17:40:15 -0400

    Initial Commit
    Changed head tags to provide necessary information, and edited the Readme.md to provide phone information.

--- Assignment1/project/js/script.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let githubLink = document.getElementById("githubLink");
-githubLink.addEventListener('click', function(){
-    window.open("https://github.com/langleynick/WEB301_Assignments");
-});
