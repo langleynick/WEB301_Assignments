6acf77c9cb02d7eaa981671551f06d6fd50981c6
langleynick <w0106967@myscc.ca>
2022-12-11 20:06:30 -0500

    Added all form fields
    Added form fields and select box with a javascript operation to fill in all provinces and territories along with their codes

--- WEB301As04/js/script.js
+++ WEB301As04/js/script.js
@@ -24,5 +24,25 @@
       
       form.classList.add('was-validated')
     }, false)
-  })
+  });
+  let select = document.getElementById("select");
+  const locations = [
+    {"name":"Alberta", "code":"AB"},
+    {"name":"British Columbia", "code":"BC"},
+    {"name":"Manitoba", "code":"MN"},
+    {"name":"New Brunswick", "code":"NB"},
+    {"name":"Newfoundland and Labrador", "code":"NL"},
+    {"name":"Nova Scotia", "code":"NS"},
+    {"name":"Nunavut", "code":"NV"},
+    {"name":"Ontario", "code":"ON"},
+    {"name":"Prince Edward Island","code":"PEI"},
+    {"name":"Quebec", "code":"QC"},
+    {"name":"Saskatchewan","code":"SK"},
+    {"name":"Yukon", "code":"YK"}
+  ];
+  let options = "<option selected>Select your Province/Territory</option>";
+  locations.forEach(location => {
+    options += '<option value="' + location.code + '">' + location.name + '</option>';
+  });
+  select.innerHTML= options;
 })()
